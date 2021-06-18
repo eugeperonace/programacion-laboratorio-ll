c937ad00f380b8df410b46afa6491c2bcbc556b4
unknown <eugeperonace@gmail.com>
2021-06-17 23:07:08 -0300

    Estantes-Productos

---
+++ Clases/Clase6_Repaso_Estantes/TestEstante/Biblioteca/Estante.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) :this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante estante)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"UBICACION ESTANTE: {estante.ubicacionEstante}");
+            sb.AppendLine("PRODUCTOS DEL ESTANTE:");
+            foreach (Producto p in estante.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(p));
+            }
+            return sb.ToString();
+        }
+
+
+        public static bool operator -(Estante estante, Producto producto)
+        {
+            bool retorno = false;
+            if (!(estante is null) && !(producto is null))
+            {
+                Producto[] productosAux = estante.GetProductos();
+                for (int i = 0; i < estante.GetProductos().Length; i++)
+                {
+                    if (productosAux[i] == producto)
+                    {
+                        productosAux[i] = null;
+                        retorno = true;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        //productosAux.Count();
+        public static bool operator +(Estante estante, Producto producto)
+        {
+
+            if (estante != producto) //si no se existe ese producto en mi estante
+            {
+                Producto[] productosAux = estante.GetProductos();
+                for (int i = 0; i < estante.GetProductos().Length; i++)
+                {
+                    if (productosAux[i] is null) //si hay espacio
+                    {
+                       productosAux[i] = producto;
+                       return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+            //for (int i = 0; i < estante.productos.Length; i++)
+            //{
+            //    if(estante.productos[i] == producto)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+
+            bool retorno = false;
+
+            if (!(estante is null))
+            {
+                foreach (Producto prod in estante.productos)
+                {
+                    if (prod == producto)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+    }
+}
---
+++ Clases/Clase6_Repaso_Estantes/TestEstante/Biblioteca/Producto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarras;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = codigo;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"MARCA: {p.marca}");
+            sb.AppendLine($"CÓDIGO: {p.codigoDeBarras}");
+            sb.AppendLine($"PRECIO: {p.precio}");
+
+            return sb.ToString();
+        }
+
+        //codigoDeBarras es privado y como no le declaramos un getter, si lo queremos usar desde el Main no podemos. La única forma de obtenerlo es a través de una conversión explícita.
+        //Convierto un producto a string. convierto un producto a su codigo de barras
+      
+        public static explicit operator string(Producto producto) 
+        {
+            return producto.codigoDeBarras;
+        }
+
+        public static bool operator !=(Producto producto, string marca)
+        {
+            return !(producto == marca);
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public static bool operator ==(Producto producto, string marca)
+        {
+            bool retorno = false;
+            
+            if (!(producto is null) && !(marca is null))
+            {
+                if (producto.marca == marca)
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+
+            if (!(p1 is null) && !(p2 is null))
+            {
+                if ((p1.marca == p2.marca) && (p1.codigoDeBarras == p2.codigoDeBarras))
+                {
+                    retorno = true;
+                }
+            }
+            return retorno;
+        }
+
+        // return (p1 == p2.marca && p1.codigoDeBarras == p2.codigoDeBarras);
+    }
+}
+
+
+
